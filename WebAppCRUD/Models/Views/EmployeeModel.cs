0c01628418a98ea07258cf8c1f6945bea4492a3f
Zaw Myo Hein <zmyohein09@gmail.com>
2024-12-04 18:29:15 +0800

    Add Project

--- WebAppCRUD/Models/Views/EmployeeModel.cs
+++ WebAppCRUD/Models/Views/EmployeeModel.cs
@@ -1,6 +1,36 @@
-﻿namespace WebAppCRUD.Models.Views
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppCRUD.Models.Views
 {
     public class EmployeeModel
     {
+        public int Id { get; set; }
+
+        [Required, Display(Name = "Employee No")]
+        public required string EmployeeNo { get; set; }
+        [Required, Display(Name = "Employee Name")]
+        public required string EmployeeName { get; set; }
+        [Required]
+        public required string Occupation { get; set; }
+        [Required]
+        public required string Department { get; set; }
+        [Required, DisplayFormat(ApplyFormatInEditMode = true)]
+        [Display(Name ="Hired Date")]
+        public required DateOnly HiredDate { get; set; }
+        [Required, DisplayFormat(ApplyFormatInEditMode = true)]
+        public required DateOnly TerminationDate { get; set; }
+        [Required, DisplayFormat(ApplyFormatInEditMode = true)]
+
+        [Display(Name ="Date Of Birth")]
+        public required DateOnly DOB { get; set; }
+        [Display(Name ="Mobile")]
+        public required string ContactNumber { get; set; }
+        [Display(Name ="Contact Address")]
+        public required string ContactAddress { get; set; }
+        [Display(Name ="IC Number")]
+        public required string ICNumber { get; set; }
+        public required string Gender { get; set; }
+        public required string Title { get; set; }
+        public string? Photo { get; set; }
     }
 }
--- WebAppCRUD/Program.cs
+++ WebAppCRUD/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppCRUD.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register DbContext
+builder.Services.AddDbContext<CRUDDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
